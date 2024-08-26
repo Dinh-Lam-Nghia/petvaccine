58abde560bc8e8829ba357f2c762f64e11e4aae9
ThovanCong <thovancong123789@gmail.com>
2024-08-26 09:18:28 +0700

    repo

---
+++ src/main/java/fpt/fa/repository/PetOwnersRepository.java
@@ -0,0 +1,23 @@
+package fpt.fa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fpt.fa.entity.PetOwners;
+
+@Repository
+public interface PetOwnersRepository extends JpaRepository<PetOwners, Integer> { 
+	List<PetOwners> findByDelete(int delete);
+	List<PetOwners> findAllByOwnerNameContainingOrPhoneNumberContainingOrEmailContainingOrAddressContainingAndDelete(String ownerName, String phoneNumber, String email, String address, int delete);
+	long countByDelete(int delete);
+	List<PetOwners> findTop10ByDeleteOrderByDateOfRegistrationDesc(int delete);
+
+//	@Modifying
+//	@Query("UPDATE PetOwners SET address = :address, dateOfRegistration = :dateOfRegistration, isDelete = :delete, email = :email, ownerName = :ownerName, phoneNumber = :phoneNumber WHERE ownerID = :ownerID")
+//	void updateById(@Param("ownerID") int ownerID, @Param("ownerName") String ownerName,
+//			@Param("phoneNumber") String phoneNumber, @Param("email") String email, @Param("address") String address,
+//			@Param("dateOfRegistration") Date dateOfRegistration, @Param("delete") int delete);
+
+}
